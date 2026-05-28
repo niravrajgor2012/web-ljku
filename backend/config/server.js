dd7b3eee9fa7c85206f0cd5e867a7c986a3ed249
devhind <apjcodewarriors2026@gmail.com>
2026-05-28 12:05:39 +0530

    dd

--- backend/config/server.js
+++ backend/config/server.js
@@ -3,6 +3,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: parseInt(env('PORT', '1337')),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env('APP_KEYS', '').split(','),
   },
